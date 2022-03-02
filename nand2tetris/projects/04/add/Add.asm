8521f96e5479601c86c2e5105ee624b2c16a0e01
Floyd Hightower <floyd.hightower27@gmail.com>
2022-03-02 06:52:15 -0500

    Adding simple program to get started

---
+++ nand2tetris/projects/04/add/Add.asm
@@ -0,0 +1,14 @@
+// Add R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+//
+// This program only needs to handle arguments that satisfy
+// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+
+@0
+D=M
+
+@1
+D=D+M
+
+@2
+M=D
