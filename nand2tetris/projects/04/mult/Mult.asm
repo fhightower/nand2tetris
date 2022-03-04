640f5802bdc985b4dc9357d023f35957f527ab82
Floyd Hightower <floyd.hightower27@gmail.com>
2022-03-04 08:05:55 -0500

    Initial commit of working mult script

--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -9,4 +9,56 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// In this code:
+// `i` is the counter
+
+    // i = 0
+    @i
+    M=0
+
+    // R2 = 0
+    @2
+    M=0
+
+    // check if R0 is zero (and end if so)
+    @0
+    D=M
+    @END
+    D;JEQ
+
+    // check if R1 is zero (and end if so)
+    @1
+    D=M
+    @END
+    D;JEQ
+(LOOP)
+    // i++
+    @i
+    M=M+1
+
+    // D = R0
+    @0
+    D=M
+
+    // R2 += D
+    @2
+    M=M+D
+
+    // D = R1
+    @1
+    D=M
+
+    // D -= i
+    @i
+    D=D-M
+
+    // end if i == R1
+    @END
+    D;JEQ
+    // otherwise, continue looping
+    @LOOP
+    0;JMP
+(END)
+    // infinite loop to signal end of program
+    @END
+    0;JMP
