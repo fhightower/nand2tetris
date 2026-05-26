bb8faee02e40360b92d7f281aea79dfecd08b35d
Floyd Hightower <floyd.hightower27@gmail.com>
2026-05-26 06:52:23 -0400

    Add code generator

---
+++ nand2tetris/projects/07/codeGenerator.go
@@ -0,0 +1,11 @@
+package main
+
+func GenerateAssembly(cmds []VmCommand) ([]string, error) {
+	var assembly []string
+
+	for _, cmd := range cmds {
+		// todo: start here and write this function
+	}
+
+	return assembly, nil
+}
