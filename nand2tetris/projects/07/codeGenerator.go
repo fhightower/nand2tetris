2e03f3c628a0b00170c4df762d7232291cdc44bf
Floyd Hightower <floyd.hightower27@gmail.com>
2026-06-19 06:07:10 -0400

    Implement gt

--- nand2tetris/projects/07/codeGenerator.go
+++ nand2tetris/projects/07/codeGenerator.go
@@ -55,6 +55,33 @@ func handleArithmeticCommand(cmd VmCommand, labelCounter int) ([]string, int, er
 			fmt.Sprintf("(%s)", endLabel),
 		}
 		return result, labelCounter + 1, nil
+	case "gt":
+		trueLabel := fmt.Sprintf("TRUE_%d", labelCounter)
+		endLabel := fmt.Sprintf("END_%d", labelCounter)
+
+		result := []string{
+			"@SP",
+			"AM=M-1",
+			"D=M",
+			"A=A-1",
+			"D=M-D",
+			// this ^ is subtraction
+			fmt.Sprintf("@%s", trueLabel),
+			"D;JGT",
+			// False path:
+			"@SP",
+			"A=M-1",
+			"M=0",
+			fmt.Sprintf("@%s", endLabel),
+			"0;JMP",
+			// True path:
+			fmt.Sprintf("(%s)", trueLabel),
+			"@SP",
+			"A=M-1",
+			"M=-1",
+			fmt.Sprintf("(%s)", endLabel),
+		}
+		return result, labelCounter + 1, nil
 		// todo: start here and build this case statement out
 	}
 	return nil, labelCounter, fmt.Errorf("unsupported arithmetic command: %s", cmd.Arg1)
